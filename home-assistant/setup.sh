03b54f8ca6d4da653f48f17deff48330a7e4a762
Nat Burns <nbaccount@burnskids.com>
2019-12-07 22:33:14 -0500

    Miscellaneous updates.

--- home-assistant/setup.sh
+++ home-assistant/setup.sh
@@ -1,3 +1,4 @@
+sudo apt install systemd-docker
 sudo cp /storage/workspaces/media-server-setup/home-assistant/home-assistant@homeassistant.service /etc/systemd/system/home-assistant@homeassistant.service
 sudo systemctl enable home-assistant@homeassistant.service
 sudo systemctl start home-assistant@homeassistant.service
--- webscreens/setup.sh
+++ webscreens/setup.sh
@@ -1,4 +1,5 @@
 sudo docker build -t burnnat/webscreens docker
+sudo apt install systemd-docker
 sudo cp /storage/workspaces/media-server-setup/webscreens/webscreens.service /etc/systemd/system/webscreens.service
 sudo systemctl enable webscreens.service
 sudo systemctl start webscreens.service
