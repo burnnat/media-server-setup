bc0d4c79a7dd3fbccbd22e14b84dd00f58865efa
Nat Burns <nbaccount@burnskids.com>
2020-02-16 15:44:44 -0500

    Add homeassistant user to docker for git commands.

--- home-assistant/upgrade.sh
+++ home-assistant/upgrade.sh
@@ -1,3 +1,3 @@
 sudo docker pull homeassistant/home-assistant:latest
-sudo docker build -t burnnat/home-assistant docker
+sudo docker build --build-arg UID=$(id -u homeassistant) --build-arg GID=$(id -g homeassistant) -t burnnat/home-assistant docker
 sudo systemctl restart home-assistant@homeassistant.service 
