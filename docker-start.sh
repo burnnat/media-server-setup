edb2829af75d8861e15cd5bab880eaefe0348c93
Nat Burns <nbaccount@burnskids.com>
2020-04-03 12:53:24 -0400

    Add script to start Docker services.

---
+++ docker-start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo systemctl start home-assistant@homeassistant.service
+sudo systemctl start webscreens.service
+sudo systemctl start esphome.service
+sudo systemctl start code-server.service
