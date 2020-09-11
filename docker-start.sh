44c13d3f9884f54c72de731631abce934ea34fe9
Nat Burns <nbaccount@burnskids.com>
2020-09-11 09:24:44 -0400

    Add influxDB to docker start.

--- docker-start.sh
+++ docker-start.sh
@@ -3,3 +3,4 @@ sudo systemctl start home-assistant@homeassistant.service
 sudo systemctl start webscreens.service
 sudo systemctl start esphome.service
 sudo systemctl start code-server.service
+sudo systemctl start influxdb.service
