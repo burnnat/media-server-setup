7087ac652a6d4f4a22005bcab7ce55f1de672fb3
Nat Burns <nbaccount@burnskids.com>
2020-12-08 02:26:29 -0500

    Update reverse proxy config.

--- docker-update.sh
+++ docker-update.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 ./docker-stop.sh "$1"
 
-sudo docker-compose pull --quiet --ignore-pull-failures
-sudo docker-compose build --pull
+(
+	cd $1
+	sudo docker-compose pull --quiet --ignore-pull-failures
+	sudo docker-compose build --pull
+)
 
 ./docker-start.sh "$1"
