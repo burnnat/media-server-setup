1ed5b061289c754ef5158b19abdf85456ae4894d
Nat Burns <nbaccount@burnskids.com>
2020-12-09 21:32:52 -0500

    Add homeassistant to docker compose.

--- home-assistant/setup.sh
+++
@@ -1,4 +0,0 @@
-sudo apt install systemd-docker
-sudo cp /storage/workspaces/media-server-setup/home-assistant/home-assistant@homeassistant.service /etc/systemd/system/home-assistant@homeassistant.service
-sudo systemctl enable home-assistant@homeassistant.service
-sudo systemctl start home-assistant@homeassistant.service
--- home-assistant/upgrade.sh
+++
@@ -1,3 +0,0 @@
-sudo docker pull homeassistant/home-assistant:latest
-sudo docker build --build-arg UID=$(id -u homeassistant) --build-arg GID=$(id -g homeassistant) -t burnnat/home-assistant docker
-sudo systemctl restart home-assistant@homeassistant.service 
