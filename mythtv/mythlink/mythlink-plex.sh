a77cc0b3dd70599e5a8b6035249250de0226f955
Nat Burns <nbaccount@burnskids.com>
2021-09-02 22:30:55 -0400

    Add mythlink.

---
+++ mythtv/mythlink/mythlink-plex.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Setup MythTV connection info (force TCP connection for Docker by using 127.0.0.1)
+mkdir -p /root/.mythtv
+sed "s/localhost/127.0.0.1/g" /etc/mythtv/config.xml > /root/.mythtv/config.xml
+
+# Setup storage directory
+rm -r /storage/tv/shows
+mkdir /storage/tv/shows
+
+# Copy episode-based shows
+/root/mythlink.pl --link /storage/tv/temp --format "%Ct/S%ss/%T/Season %ss/%T - S%ssE%ep - %S" --separator "#"
+rm -r /storage/tv/temp/2/S
+cp -r /storage/tv/temp/2/*/* /storage/tv/shows
+rm -r /storage/tv/temp
+
+# Copy date-based shows
+/root/mythlink.pl --link /storage/tv/temp --format "%Ct/S%ss/%T/Season %oY/%T - %oY-%om-%od - %S" --separator "#"
+cp -r /storage/tv/temp/2/S/* /storage/tv/shows
+rm -r /storage/tv/temp
+
+# Copy TV movies
+/root/mythlink.pl --link /storage/tv/temp --format "%Ct/%T/%T"
+rm -r /storage/tv/movies
+mv /storage/tv/temp/1 /storage/tv/movies
+rm -r /storage/tv/temp
